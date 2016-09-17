1e403f0aab076e75433fa5bf3f034c4b82fb3d7a
SunnyCat2013 <lizhenyang_2008@163.com>
2016-09-17 09:53:38 +0800

    use redux 'createStore' instead of my brief implementation

--- react-redux/counter.js
+++ react-redux/counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch(action.type) {
@@ -13,24 +13,25 @@ const counter = (state = 0, action) => {
   }
 }
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-  const getState = () => state;
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.map(l => l());
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners.filter(l !== listener);
-    }
-  }
-  dispatch({});
-  return {getState, dispatch, subscribe};
-}
+// // a brief implementation of createStore
+// const createStore = (reducer) => {
+//   let state;
+//   const listeners = [];
+//   const getState = () => state;
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.map(l => l());
+//   }
+//
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return () => {
+//       listeners.filter(l !== listener);
+//     }
+//   }
+//   dispatch({});
+//   return {getState, dispatch, subscribe};
+// }
 
 const store = createStore(counter);
 const render = () => {
